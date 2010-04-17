bafd7c1b708e0dc221fbdc5fdd237cb8cbdf2efa
David <davidb@csh.rit.edu>
2010-04-16 21:40:35 -0400

    GameBoard->XML and XML->GameBoard works now.
    board_objects.py
        -Fixed some stupid mistakes
    
    game_board.py
        -Can now create a blank game board and add objects
    
    parser.py
        -Parsing XML now works

--- parser.py
+++ parser.py
@@ -2,28 +2,52 @@
 
 from xml.sax import saxutils
 from board_objects import *
+from game_board import *
 # Parser module for UVB Game Board XML
 
 class ParseGameBoard(saxutils.DefaultHandler):
     def __init__(self):
-        pass
+        self.board = None
 
     def startElement(self, name, attrs):
         # python lacks switch-case, so use if-elif
-        x = attrs.get('x',None)
-        y = attrs.get('y',None)
-        if(name=="Snowball"):
-            pass
-        elif(name=="BoardObstacle"):
-            pass
-        elif(name=="Snowman"):
-            pass
-        elif(name=="Player"):
-            name = attrs.get('name',None)
-            print "Player at x: " + str(x) + " y: " + str(y) + " name: " + name
+        if(name=="board"):
+            width  = int( attrs.get('width',None) )
+            height = int( attrs.get('height',None) )
+            self.board = GameBoard(width,height)
+        else:
+            x = int( attrs.get('x',None) )
+            y = int( attrs.get('y',None) )
+            if(name=="Snowball"):
+                direction = attrs.get('direction',None)
+                self.board.addObject( Snowball(x,y,direction) )
+            elif(name=="BoardObstacle"):
+                self.board.addObject( BoardObstacle(x,y) )
+            elif(name=="Snowman"):
+                self.board.addObject( Snowman(x,y) )
+            elif(name=="Player"):
+                name = attrs.get('name',None)
+                self.board.addObject( Player(x,y,name) )
 
 from xml.sax import make_parser
 from xml.sax.handler import feature_namespaces
+from board_objects import *
+from game_board import *
+
+class GameBoardParser(object):
+    def __init__(self):
+        # Create a parser
+        self.parser = make_parser()
+        # Tell the parser we are not interested in XML namespaces
+        self.parser.setFeature(feature_namespaces, 0);
+        # Create the handler
+        self.dh = ParseGameBoard()
+        self.parser.setContentHandler(self.dh)
+
+    def parse(self,XMLstring):
+        self.parser.parseString(XMLstring)
+        print dh.board.__str__()
+        return self.dh.board
 
 if __name__ == '__main__':
     # Create a parser
@@ -31,6 +55,7 @@ if __name__ == '__main__':
     # Tell the parser we are not interested in XML namespaces
     parser.setFeature(feature_namespaces, 0);
     # Create the handler
-    dh = ParseGameBoard(board)
+    dh = ParseGameBoard()
     parser.setContentHandler(dh)
     parser.parse('test.xml')
+    print dh.board.__str__()
