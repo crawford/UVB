9929bdc7320a64589bd5655a96821fd13e16fbb5
Alex <adrofwarc@gmail.com>
2010-04-18 02:44:00 -0400

    Wrote getconnections functionality

--- server.py
+++ server.py
@@ -39,51 +39,58 @@ import select
 import socket
 import sys
 import threading
+import datetime
 
 class Server:
     def __init__(self):
-        self.host = ''
-        self.port = 88888
+        self.host = socket.gethostname()
+        self.port = 41294
         self.backlog = 5
         self.size = 1024
         self.server = None
-        self.threads = []
+        self.connections = []
+        self.server_timeout = 5
+        self.start_waittime = 300
 
-    def open_socket(self):
+    def open_server(self):
         try:
             self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.server.bind((self.host,self.port))
-            self.server.listen(5)
+            self.server.listen(self.backlog)
         except socker.error, (value,message):
             if self.server:
                 self.server.close()
             print "Could not open socket: " + message
             sys.exit(1)
 
-    def run(self):
-        self.open_socket()
+    def getconnections(self, waittime):
+        starttime = datetime.datetime.now() + waittime
         input = [self.server,sys.stdin]
         running = 1
-        while running:
-            inputready,outputready,exceptready = select.select(input,[],[])
+        while running and starttime > datetime.datetime.now():
+            inputready,outputready,exceptready = select.select(input,[],[], self.server_timeout)
 
             for s in inputready:
                 if s == self.server:
                     # handle the server socket
-                    c = Client(self.server.accept())
+                    c = Connection(self.server.accept())
                     c.start()
-                    self.threads.append(c)
+                    self.connections.append(c)
                 elif s == sys.stdin:
                     # handle standard input
                     junk = sys.stdin.readline()
                     running = 0
-        # close all threads
-
+                    
         self.server.close()
+
+    def run(self):
+        self.open_server()
+        self.getconnections(self.start_waittime)
+
         for c in self.threads:
             c.join()
 
-class Client(threading.Thread):
+class Connection(threading.Thread):
     def __init__(self,(client,address)):
         threading.Thread.__init__(self)
         self.client = client
