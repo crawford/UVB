76ef160b79139f653c80940a52a1d2aad5debab7
Robert Schriver <schriver@rancor.csh.rit.edu>
2010-04-18 02:07:56 -0400

    fuck

--- client.py
+++ client.py
@@ -34,6 +34,7 @@ Players win by:
 # Main module
 
 from game_board import *
+from random import choice
 
 SERVER = "blahblahblah"
 PORT = "blah"
@@ -60,12 +61,38 @@ class Client
 		for boardObject in objects :
 			if boardObject.__class__ is BoardObstacle 
 				&& !contains( obstacleList , lambda x : x == boardObject ) :
-				obstacleList.add( boardObject )
+				obstacleList.add( ( boardObject.x , boardObject.y ) )
 			else if boardObject.__class__ is Player : 
 				if getDistance( boardObject.x , boardObject.y ) < currClosest :
 					closestPlayer = boardObject
+		
+		direction = getDirection( closestPlayer.x , closestPlayer.y )
+ 
+		directionSum = direction[0] + direction[1]
 
+		if directionSum == direction[0] :
+			possibleMoves.add( direction[0] , direction[1] )
+			possibleMoves.add( direction[0] , 1 )
+			possibleMoves.add( direction[0] ,  - 1 )
+		else if directionSum == direction[1] :
+			possibleMoves.add( direction[0] , direction[1] )
+			possibleMoves.add( 1 , direction[1] )
+			possibleMoves.add( -1 , direction[1] )
+		else
+			possibleMoves.add( direction[0] , direction[1] )
+			possibleMoves.add( direction[0] , 0 )
+			possibleMoves.add( 0 , direction[1] )
 
+		for move in possibleMoves : 
+			if !contains( obstacleList , lambda x : x == move ) :
+				possibleMoves.remove( move )
+		
+		move = choice( possibleMoves )
+
+		x = move[0]
+		y = move[1]
+
+		return (x , y) 
 				
 			
 	def getDistance(x , y) :
@@ -74,12 +101,12 @@ class Client
 	def getDirection(x , y) :
 		if xPos - x == 0 :
 			if  yPos - y == 0 : 
-				return 0 , 0
-			else return 0 , yPos - y / Math.abs( yPos - y )
+				return ( 0 , 0 )
+			else return ( 0 , yPos - y / Math.abs( yPos - y ) )
 		else 
 			if yPos - y == 0 :
-				return xPos - x / Math.abs( xPos - x ) , 0
-			else return xPos - x / Math.abs( xPos - x ) , yPos - y / Math.abs( yPos - y )
+				return ( xPos - x / Math.abs( xPos - x ) , 0 ) 
+			else return ( xPos - x / Math.abs( xPos - x ) , yPos - y / Math.abs( yPos - y ) )
 
 	def contains(list , filter)
 		for x in list : 
