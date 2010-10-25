e4febc2fc9aba794c52213874bbff84bb5c37c3f
Alex Crawford <adrofwarc+github@gmail.com>
2010-10-24 19:15:26 -0500

    Removed keys.  Added other client

--- server/testclient2.py
+++ server/testclient2.py
@@ -9,7 +9,14 @@ def dummy(board):
 
 	print board
 
-	return (Action.THROWSNOWBALL, Direction.SOUTHEAST)
+	obj = 1
+	while obj:
+		next_pos = board.next_pos_in_direction((0, 0), last_dir)
+		obj = board.get_object_at(next_pos)
+		if obj:
+			last_dir = random.randint(0, 7)
+
+	return (Action.MOVE, last_dir)
 
 
 
